cf9fb67a3f29dbf630a7346415cf80a31e3324dc
lilbunnyfufuu <thegunners.ptd@gmail.com>
2023-11-04 21:59:22 +0700

    initial commit

---
+++ app/server.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, render_template
+from flask_cors import CORS
+
+from predict import predict_sentiments
+from youtube import get_video_comments
+
+app = Flask(__name__)
+CORS(app)
+
+
+def get_video(video_id):
+    if not video_id:
+        return {"error": "video_id is required"}
+
+    comments = get_video_comments(video_id)
+    predictions = predict_sentiments(comments)
+
+    positive = predictions.count("Positive")
+    negative = predictions.count("Negative")
+
+    summary = {
+        "positive": positive,
+        "negative": negative,
+        "num_comments": len(comments),
+        "rating": round((positive / len(comments)) * 100, 2)
+    }
+
+    return {"predictions": predictions, "comments": comments, "summary": summary}
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    summary = None
+    comments = []
+    if request.method == 'POST':
+        video_url = request.form.get('video_url')
+        video_id = video_url.split('?v=')[1].split('&list=')[0].split('&index=')[0].split('&t=')[0].split('&ab_channel=')[0]
+        data = get_video(video_id)
+
+        summary = data['summary']
+        comments = list(zip(data['comments'], data['predictions']))
+    return render_template('index.html', summary=summary, comments=comments)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
